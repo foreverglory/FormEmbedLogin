e62fcf29411391869e6abec2ed1f8e8105e5985e
ForeverGlory <foreverglory@qq.com>
2014-09-25 17:41:29 +0800

    增加 消息提示、关闭按钮

--- form_embed_login.js
+++ form_embed_login.js
@@ -1,9 +1,15 @@
 (function ($) {
     Drupal.behaviors.form_embed_login = {
         attach: function (context) {
-            $(".embed-login", context).hide().prev().on("click", function () {
-                $(this).next(".embed-login").show();
-                return false;
+            $(".embed-login", context).each(function () {
+                var $form = $(this).hide();
+                $(".embed-login-close", $form).on("click", function () {
+                    $form.hide();
+                });
+                $form.prev().on("click", function () {
+                    $form.show();
+                    return false;
+                });
             });
         }
     };
